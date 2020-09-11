623ad57170621f5a4af1c6bde5feb6e0e0ed848d
M-Alaa96 <70509609+M-Alaa96@users.noreply.github.com>
2020-09-11 03:51:12 +0200

    Update Node.java

--- src/huffman/Node.java
+++ src/huffman/Node.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package huffman;
 
+package huffman;
 import java.util.Comparator;
 
-/**
- *
- * @author RS
- */
 public class Node {
 
     Node left, right;
